43a313e458779dc7de4f32a82cd8cb574f5801a1
AlexeiLuchian <alexeiluchianx@gmail.com>
2025-11-11 01:21:10 +0200

    Add more search related tests

--- tests/test_search.py
+++ tests/test_search.py
@@ -2,6 +2,7 @@ import pytest
 from selenium.webdriver.common.by import By
 from utils.driver_setup import DriverSetup
 from pages.base_page import BasePage
+from urllib.parse import quote
 
 class TestSearch:
     @pytest.fixture(autouse=True)
@@ -11,7 +12,40 @@ class TestSearch:
         self.page = BasePage(self.driver)
         yield
         self.driver.quit()
-    
+
+    def test_search_result_count(self):
+        search_query = "pastile"
+        result_count_locator = (By.CSS_SELECTOR, "div.mb-3 span.flex-shrink-0")
+        search_box_locator = (By.ID, "header-search-input")
+        search_button_locator = (By.ID, "header-search__button-submit")
+        accept_cookies = (By.ID, "CybotCookiebotDialogBodyLevelButtonLevelOptinAllowAll")
+        
+        self.page.click(accept_cookies)
+        self.page.type_text(search_box_locator, search_query)
+        self.page.click(search_button_locator)
+        self.page.find_element(result_count_locator)
+        result_count = self.page.find_element(result_count_locator).text.strip('()').split()[0]
+
+        assert result_count.isnumeric()
+        print(f"✓ Result count: {result_count}")
+
+    def test_search_suggestions(self):
+        '''
+        Tests if suggestions appear after typing something into the search box.
+        The length of the search query should be 2 characters long or more.
+        '''
+        search_query = "pa"
+        search_box_locator = (By.ID, "header-search-input")
+        suggestions_locator = (By.CSS_SELECTOR, "div.aa-suggestion")
+        accept_cookies = (By.ID, "CybotCookiebotDialogBodyLevelButtonLevelOptinAllowAll")
+        
+        self.page.click(accept_cookies)
+        self.page.type_text(search_box_locator, search_query)
+
+        assert self.page.find_element(suggestions_locator)
+        print("✓ Search suggestions test passed!")
+
+
     def test_search_functionality(self):
         search_query = "sampon"
         search_box_locator = (By.ID, "header-search-input")
@@ -37,3 +71,18 @@ class TestSearch:
         assert placeholder is not None
         assert len(placeholder) > 0
         print(f"✓ Placeholder text: '{placeholder}'")
+
+    def test_special_characters_search(self):
+        search_query = "șampon"
+        search_box_locator = (By.ID, "header-search-input")
+        search_button_locator = (By.ID, "header-search__button-submit")
+        accept_cookies = (By.ID, "CybotCookiebotDialogBodyLevelButtonLevelOptinAllowAll")
+
+        self.page.click(accept_cookies)
+        self.page.type_text(search_box_locator, search_query)
+        self.page.click(search_button_locator)
+
+        search_query_url = quote(search_query)
+        self.page.wait_for_url_contains(search_query_url)
+        assert search_query_url.lower() in self.driver.current_url.lower()
+        print("✓ Special characters search test passed!")
